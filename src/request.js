9255f7c8169be2fcecc2fa0fdafbc2160750fa7a
Amdebirhan <amdebirhanteshome@gmail.com>
2022-04-20 10:48:25 -0700

    secure

--- src/request.js
+++ src/request.js
@@ -2,7 +2,7 @@ const APIKEY = '96fa71b968f3c42e1056cee681997991'
 
 const requsts = {
     fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
-    fetchNetflixOrginals: `/discover/tv/?api_key=${APIKEY}&with_networks=213`,
+    fetchNetflixOrginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_geners=28`,
     fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_geners=35`,
